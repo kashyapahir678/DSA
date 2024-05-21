f8e238e8174bbb0db54df9aedd8d85d5579f0047
KashyapAhir <kashyapahir786@gmail.com>
2024-05-21 11:16:46 +0530

    change

--- circularQueue.c
+++ circularQueue.c
@@ -1,66 +1,78 @@
-// Online C compiler to run C program online
-#include <stdio.h>
-
-int rare=-1;
-int front=-1;
-
-int circularInsert(int arr[],int n,int element){
-    
-    if(front==-1 && rare==-1){
-        front=rare=0;
-        arr[rare]=element;
-    }
-    else if(((rare+1)%n)==front){
-        printf("queue is full");
-    }
-    else{
-        rare=(rare+1)%n;
-        arr[rare]=element;
-    }
-}
-
-int circularDelete(int arr[],int n){
-    
-    if(front==-1 && rare == -1){
-        printf("queue is full");
-    }
-    else if(front == rare){
-        front=rare=-1;
-    }
-    else{
-        printf("%d",arr[front]);
-        rare=(front+1)%n;
-    }
-}
-
-int circularDisplay(int arr[],int n){
-    
-    int i=front;
-    
-    if(front==-1 && rare==-1){
-        printf("queue is empty!");
-    }
-    else{
-        while(i!=rare){
-            printf("%d \t",arr[i]);
-            i=((i+1)%n); 
-        }
-    }
-    printf("%d",arr[rare]);
-}
-
-int main() {
-   
-    int n=5;
-    int arr[n];
-    
-    circularInsert(arr,n,23);
-    circularInsert(arr,n,42);
-    circularInsert(arr,n,12);
-    circularInsert(arr,n,56);
-    circularInsert(arr,n,86);
-    circularDelete(arr,n);
-    circularDisplay(arr,n);
-
-    return 0;
+#include <stdio.h> 
+ 
+int front = -1; 
+int rear = -1; 
+ 
+void enqueue(int x, int n, int queue[]) 
+{ 
+    if (front == -1 && rear == -1) 
+    { 
+        front = rear = 0; 
+        queue[rear] = x; 
+    } 
+    else if (((rear + 1) % n) == front) 
+    { 
+        printf("Queue is full"); 
+    } 
+    else 
+    { 
+        rear = (rear + 1) % n; 
+        queue[rear] = x; 
+    } 
+} 
+void dequeue(int queue[], int n) 
+{ 
+    if (front == -1 && rear == -1) 
+    { 
+        printf("queue is empty"); 
+    } 
+    else if (front == rear) 
+    { 
+        front = rear = -1; 
+    } 
+    else 
+    { 
+        printf("%d\n", queue[front]); 
+        front = (front + 1) % n; 
+    } 
+} 
+ 
+void desplay(int queue[], int n) 
+{ 
+    int i = front; 
+    if (front == -1 && rear == -1) 
+    { 
+        printf("data not found"); 
+    } 
+    else 
+    { 
+        while (i != rear) 
+        { 
+            printf("%d\t", queue[i]); 
+            i = (i + 1) % n; 
+        } 
+        printf("%d", queue[rear]); 
+        printf("\n"); 
+    } 
+} 
+int main() 
+{ 
+    int n = 5; 
+    int queue[n]; 
+    enqueue(10, n, queue); 
+    enqueue(20, n, queue); 
+    enqueue(-1, n, queue); 
+    enqueue(18, n, queue); 
+    enqueue(15, n, queue); 
+    desplay(queue, n); 
+ 
+    dequeue(queue, n); 
+    dequeue(queue, n); 
+ 
+    desplay(queue, n); 
+    enqueue(35, n, queue); 
+    enqueue(45, n, queue); 
+    desplay(queue, n); 
+ 
+    return 0; 
 }
